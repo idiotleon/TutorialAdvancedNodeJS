85af2355b3d3a65fb4c5d9d9b69b9dd2c013edee
Leon Lu <yanglu@microsoft.com>
2018-09-02 10:58:24 -0700

    demo: Keeping the callback style while adding a promise interface to it
    Node official method for working with async code is callbacks, and developers using your code might assume that you have a callback interface
    
    Adding a promise interface makes your code a lot easier to work with when there is a need to loop over an async function

--- async-promise.js
+++ async-promise.js
@@ -1,22 +1,34 @@
 const fs = require('fs');
 
-const readFileAsArray = function (file) {
+const readFileAsArray = function (file, cb = () => { }) {
     return new Promise((resolve, reject) => {
         fs.readFile(file, function (err, data) {
             if (err) {
-                return reject(err);
+                reject(err);
+                return cb(err);
             }
 
             const lines = data.toString().trim().split('\n');
             resolve(lines);
+            cb(null, lines);
         });
     });
 }
 
+// example call with a Promise
 readFileAsArray('./numbers')
     .then(lines => {
         const numbers = lines.map(Number);
         const oddNumbers = numbers.filter(number => number % 2 === 1);
-        console.log('odd numbers count: ', oddNumbers.length);
+        console.log('odd numbers count:', oddNumbers.length);
     })
     .catch(console.error);
+
+// example call with a typical callback
+readFileAsArray('./numbers', (err, lines) => {
+    if (err) throw err;
+
+    const numbers = lines.map(Number);
+    const oddNumbers = numbers.filter(number => number % 2 === 1);
+    console.log('odd numbers count:', oddNumbers.length);
+});
