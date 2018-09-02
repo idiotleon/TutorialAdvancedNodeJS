193c4548fa9576c0e8b49d5746b0047ed81f3633
Leon Lu <yanglu@microsoft.com>
2018-09-02 10:49:26 -0700

    demo: an async node function in modern Promise style

--- async-promise.js
+++ async-promise.js
@@ -1,20 +1,22 @@
 const fs = require('fs');
 
-const readFileAsArray = function (file, cb) {
-    fs.readFile(file, function (err, data) {
-        if (err) {
-            return cb(err);
-        }
+const readFileAsArray = function (file) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, function (err, data) {
+            if (err) {
+                return reject(err);
+            }
 
-        const lines = data.toString().trim().split('\n')
-        cb(null, lines);
+            const lines = data.toString().trim().split('\n');
+            resolve(lines);
+        });
     });
 }
 
-readFileAsArray('./numbers', (err, lines) => {
-    if (err) throw err;
-
-    const numbers = lines.map(Number);
-    const oddNumbers = numbers.filter(number => number % 2 === 1);
-    console.log('odd numbers count:', oddNumbers.length);
-});
+readFileAsArray('./numbers')
+    .then(lines => {
+        const numbers = lines.map(Number);
+        const oddNumbers = numbers.filter(number => number % 2 === 1);
+        console.log('odd numbers count: ', oddNumbers.length);
+    })
+    .catch(console.error);
