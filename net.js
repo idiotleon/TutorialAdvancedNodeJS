54b8586c0b78217b542eec03f519ac2380352660
Leon <yanglyu.leon.7@gmail.com>
2018-09-04 14:33:04 -0700

    demo: Working with Multiple Sockets

--- net.js
+++ net.js
@@ -1,19 +1,25 @@
 process.stdout.write('\u001B[2]\u001B[0;0f');
 
 const server = require('net').createServer();
+let counter = 0;
+let sockets = {};
 
 server.on('connection', socket => {
+    socket.id = counter++;
+    sockets[socket.id] = socket;
+
     console.log('Client connected');
     socket.write('Welcome new client!\n');
 
     socket.on('data', data => {
-        console.log('data is:', data);
-        socket.write('data is: ');
-        socket.write(data);
+        Object.entries(sockets).forEach(([, cs]) => {
+            cs.write(`${socket.id}: `);
+            cs.write(data);
+        });
     });
 
     socket.setEncoding('utf8');
-
+    delete sockets[socket.id];
     socket.on('end', () => {
         console.log("Client disconnected");
     });
