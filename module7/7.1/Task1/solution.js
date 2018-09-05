d3eecb979ae324ca46c49e7008794e10b4606249
Leon <yanglyu.leon.7@gmail.com>
2018-09-05 10:28:49 -0700

    demo: Working with the File System

---
+++ module7/7.1/Task1/solution.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const dirname = path.join(__dirname, 'files');
+
+const files = fs.readdirSync(dirname);
+
+files.forEach(file => {
+    // Always use path.join when working with file paths 
+    // to make your code platform-agnostic
+    const filePath = path.join(dirname, file);
+    fs.stat(filePath, (err, stats) => {
+        if (err) throw err;
+
+        fs.truncate(filePath, stats.size / 2, (err) => {
+            if (err) throw err;
+        });
+    });
+});
