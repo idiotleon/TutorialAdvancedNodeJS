06440d2c162c360836007c57de92821045d5aae3
Leon <yanglyu.leon.7@gmail.com>
2018-09-05 09:24:38 -0700

    demo: Working with Routes

--- module6/server.js
+++ module6/server.js
@@ -1,10 +1,27 @@
-// server: http.Server
+const fs = require('fs');
 const server = require('http').createServer();
+const data = {};
 
 server.on('request', (req, res) => {
-    // req: http.IncomingMessage
-    // res: http.ServerResponse
-
+    switch (req.url) {
+        case '/api':
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(data));
+            break;
+        case '/home':
+        case '/about':
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(fs.readFileSync(`.${req.url}.html`));
+            break;
+        case '/':
+            res.writeHead(301, { 'Location': '/home' });
+            res.end();
+            break;
+        default:
+            res.writeHead(404);
+            res.end();
+            break;
+    }
     res.writeHead(200, { 'content-type': 'text/plain' });
     res.end('Hello World\n');
 });
