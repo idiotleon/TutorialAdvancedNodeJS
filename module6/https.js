da6a980af28f6400ed3388b4e7a96d79933a2966
Leon <yanglyu.leon.7@gmail.com>
2018-09-04 23:10:09 -0700

    demo: Working with HTTPS

---
+++ module6/https.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const server = require('https')
+    .createServer({
+        key: fs.readFileSync('./key.pem'),
+        cert: fs.readFileSync('./cert.pem'),
+    });
+
+server.on('request', (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Hello World\n');
+});
+
+server.listen(443);
