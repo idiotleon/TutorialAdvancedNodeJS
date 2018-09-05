51bc75ef319a7912368df4ab6df359d68ab42563
Leon <yanglyu.leon.7@gmail.com>
2018-09-05 11:29:39 -0700

    demo: Streaming all the things

---
+++ module8/8.1/server.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+    /*
+    fs.readFile('./big.file', (err, data) => {
+        if (err) throw err;
+
+        res.end(data);
+    });*/
+
+    const src = fs.createReadSteram('./big.file');
+    src.pipe(res);
+});
+
+server.listen(8000);
