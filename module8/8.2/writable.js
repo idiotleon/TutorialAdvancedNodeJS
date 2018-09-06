b819065d403754756dba882556ef1a93290c1d53
Leon <yanglyu.leon.7@gmail.com>
2018-09-05 22:06:32 -0700

    demo: Implementing Readable and Writable Streams

---
+++ module8/8.2/writable.js
@@ -0,0 +1,15 @@
+/*
+const { Writable } = require('stream');
+
+const outStream = new Writable({
+    write(chunk, encoding, callback) {
+        console.log(chunk.toString());
+        callback();
+    }
+});
+
+process.stdin.pipe(outStream);
+*/
+
+// Equivalently
+process.stdin.pipe(process.stdout);
