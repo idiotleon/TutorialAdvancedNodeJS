a58beb1e10aa5a802037f15c1c58e4388dd1e144
Leon <yanglyu.leon.7@gmail.com>
2018-09-05 22:06:02 -0700

    refactor: Adding comments

--- module8/8.1/server.js
+++ module8/8.1/server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
+    // Loading the big file directly into memory
     /*
     fs.readFile('./big.file', (err, data) => {
         if (err) throw err;
@@ -9,6 +10,7 @@ server.on('request', (req, res) => {
         res.end(data);
     });*/
 
+    // Streaming version
     const src = fs.createReadSteram('./big.file');
     src.pipe(res);
 });
